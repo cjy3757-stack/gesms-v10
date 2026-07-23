c47dbe8dcb0b5ad4871e8b24cba53acfb2a29c71
cjy3757-stack <cjy3757@gmail.com>
2026-07-23 16:48:55 +0900

    beta ver.
    beta ver.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gesms-v10-alpha-20260723';
-const STATIC=['./','./index.html','./data.js','./service-data.js','./office-data.js','./office-excel.js','./jszip.min.js','./finance-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png'];
+const CACHE='gesms-v10-beta-20260723';
+const STATIC=['./','./index.html','./data.js','./service-data.js','./office-data.js','./office-excel.js','./prayer-excel.js','./jszip.min.js','./finance-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
