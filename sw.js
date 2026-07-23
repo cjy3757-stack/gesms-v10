4efd3c021d02206da8660b3cbfc1072eb956eaa9
cjy3757-stack <cjy3757@gmail.com>
2026-07-23 17:43:35 +0900

    RC2 보완
    RC2 보완

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gesms-v10-beta-20260723';
+const CACHE='gesms-v10-rc2-20260723';
 const STATIC=['./','./index.html','./data.js','./service-data.js','./office-data.js','./office-excel.js','./prayer-excel.js','./jszip.min.js','./finance-data.js','./manifest.webmanifest','./logo.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
